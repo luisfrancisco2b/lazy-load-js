326774605d91243ba5191bd648ed45f7414ad71b
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-08-15 16:53:39 -0300

    feat(js): add initial lazy load logic

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const images = document.querySelectorAll(".container img");
 
 images.forEach((image) => {
-  image.src = image.src.replace("&w=10", "&w=1000");
+  image.src = image.src.replace("w=10", "w=1000");
 });
