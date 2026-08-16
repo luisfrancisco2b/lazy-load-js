81ee16ed127f5e238405ee793b8901ea15370bca
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-08-15 22:27:52 -0300

    feat(js): add complete lazy load logic

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 const images = document.querySelectorAll(".container img");
 
+// Configuração do Observer
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+
+    const image = entry.target;
+
+    image.src = image.src.replace("w=10", "w=1000");
+
+    observer.unobserve(image);
+  });
+}, {});
+
+// Ativa a observação das imagens
 images.forEach((image) => {
-  image.src = image.src.replace("w=10", "w=1000");
+  observer.observe(image);
 });
